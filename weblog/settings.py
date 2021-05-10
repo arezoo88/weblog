2e6ad993c4064bf9636565d53d06dd294a6e3ffb
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-10 16:29:55 +0430

    feat: add AUTH_USER_MODEL & account app

--- weblog/settings.py
+++ weblog/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'post.apps.PostConfig'
+    'post.apps.PostConfig',
+    'account.apps.AccountConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account.User'
