e31e041eba0c93309f2497975b21cacfc1e7ea19
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-11 12:05:08 +0430

    feat: add list_display and other methods to customize admin panel

--- post/admin.py
+++ post/admin.py
@@ -4,4 +4,8 @@ from post.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'slug', 'publish', 'status')
+    list_filter = ('publish', 'status')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}  # autocomplete slug when type title
+    ordering = ('status', '-publish')
