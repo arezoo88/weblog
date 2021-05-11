1141562e1b8a06add0af142b5be6829184247364
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-11 14:29:11 +0430

    feat: add home method got get list of posts

--- post/views.py
+++ post/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def home(request):
+    posts = Post.objects.filter(status='p')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'post/index.html', context)
