817cdd76eee10260833924fa92a8bbf8a0be35cf
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-11 10:54:27 +0430

    feat: add STATIC_ROOT for defining path of folder after collectstatic

--- weblog/settings.py
+++ weblog/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
