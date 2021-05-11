dc864f0be261743944d5c5dd2416eca0e580f2dc
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-11 16:37:57 +0430

    feat: change publish into jpublish & add JDateFieldListFilter,that to filter base on shamsi date

--- post/admin.py
+++ post/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from post.models import Post
-
+from django_jalali.admin.filters import JDateFieldListFilter
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'publish', 'status')
-    list_filter = ('publish', 'status')
+    list_display = ('title', 'slug', 'jpublish', 'status')
+    list_filter = (('publish', JDateFieldListFilter), 'status')
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug': ('title',)}  # autocomplete slug when type title
     ordering = ('status', '-publish')
