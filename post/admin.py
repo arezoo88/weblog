1d4e1ce525e62b10daaf47835457253bc9754240
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-12 10:06:47 +0430

    feat: create CategoryAdmin for displying category model into admin panel

--- post/admin.py
+++ post/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from post.models import Post
+from post.models import Post,Category
 from django_jalali.admin.filters import JDateFieldListFilter
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('position', 'title', 'slug', 'status')
+    list_filter = ('status',)
+    search_fields = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
