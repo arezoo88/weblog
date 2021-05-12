ed8666e13191e13a4efc1294b7065d737e6274f5
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-12 12:43:04 +0430

    feat(Heroku): activate django-heroku

--- weblog/settings.py
+++ weblog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +129,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.User'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
