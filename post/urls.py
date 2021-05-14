1cd8fc0721aefd56a89a2b8e7988ae53aa04a2b6
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-14 12:31:27 +0430

    feat: add url for detail of posts

--- post/urls.py
+++ post/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'post'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('post/<slug:slug>/', views.detail, name='detail'),
 ]
