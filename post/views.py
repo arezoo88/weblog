8532ca59130c801094c8df82ea9c948c23c50a1f
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-14 12:32:11 +0430

    feat: add detail method

--- post/views.py
+++ post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 
 
@@ -8,3 +8,10 @@ def home(request):
         'posts': posts
     }
     return render(request, 'post/index.html', context)
+
+
+def detail(request, slug):
+    context = {
+        'post': get_object_or_404(Post, slug=slug, status='p')
+    }
+    return render(request, 'post/detail.html', context)
