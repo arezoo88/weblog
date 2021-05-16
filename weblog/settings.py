44afebe56fd2f9af4e6a7bf16f010b5f72f5f248
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-16 16:30:11 +0430

    feat: add STATICFILES_STORAGE

--- weblog/settings.py
+++ weblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG',default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 DEBUG_PROPAGATE_EXCEPTIONS = True
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Application definition
 
 INSTALLED_APPS = [
