ae8b65ad3bb783a4852f90cb626c455fcbe5f903
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-16 16:05:12 +0430

    feat: add DEBUG_PROPAGATE_EXCEPTIONS to see the errors in the log

--- weblog/settings.py
+++ weblog/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # Application definition
 
