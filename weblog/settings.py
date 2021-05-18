dbb43cfd5f022a581cd4b0d529d4c678adaf0834
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-18 11:50:43 +0430

    fix: remove STATICFILES_DIRS

--- weblog/settings.py
+++ weblog/settings.py
@@ -126,9 +126,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = (
- BASE_DIR / 'static'
-)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
