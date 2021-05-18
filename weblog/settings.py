e69506b36aaaffd49fda8f0c53d41ef99386e3b0
arezoo88 <arezoo.darvishi69@yahoo.com>
2021-05-18 11:41:00 +0430

    feat: add STATICFILES_DIRS

--- weblog/settings.py
+++ weblog/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (
+ BASE_DIR / 'static'
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
